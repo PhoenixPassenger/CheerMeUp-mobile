b57b9784eb162edf2900dda90a8d00be0ea6f07e
Rodrigo Silva Ribeiro <rodrigo@MacBook-Pro-de-Rodrigo.local>
2020-07-05 12:29:36 -0300

    criacao da arvore do repositorio

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View} from 'react-native';
+import 'react-native-gesture-handler';
+import {NavigationContainer} from '@react-navigation/native';
+
+const App: React.FC = () => {
+  return (
+    <NavigationContainer>
+      <View />
+    </NavigationContainer>
+  );
+};
+
+export default App;
